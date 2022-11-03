98eebec426bad00533f066ebf86599b64eb299c5
Ignacy2012 <107277585+Ignacy2012@users.noreply.github.com>
2022-11-03 19:21:04 +0100

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,4 +1,4 @@
-function username() {
+function addUser() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
     window.location = "kwitter_room.html";
--- kwitter_room.js
+++ kwitter_room.js
@@ -1,1 +1,40 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBPa-lvuz90nxYUp_Zj9EmxA-PArM_ndO8",
+    authDomain: "lets-chat-24665.firebaseapp.com",
+    databaseURL: "https://lets-chat-24665-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-24665",
+    storageBucket: "lets-chat-24665.appspot.com",
+    messagingSenderId: "552551776465",
+    appId: "1:552551776465:web:0d73249eeaff4bde64c5f0",
+    measurementId: "G-SPC3RHVHSX"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+     
+welcome = localStorage.getItem("user_name");
+document.getElementById("welcome").innerHTML = "welcome " + welcome;
 
+function addRoom(){
+      room_name = document.getElementById("room_name").value;
+      localStorage.setItem("room_name", room_name);
+
+      firebase.database().ref("/").child(room_name).update({
+            purpose: "adding user"
+      });
+}
+
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+
+      //End code
+      });});}
+getData();
+
+function logout() {
+      localStorage.removeItem("user_name");
+      window.location = "index.html";
+}
