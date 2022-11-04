54df806532ae18c08565adad402a01d40463c0e7
Ignacy2012 <107277585+Ignacy2012@users.noreply.github.com>
2022-11-04 21:57:45 +0100

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -27,6 +27,9 @@ function addRoom(){
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
+       console.log("Room_name " + Room_names);
+       row = "<div class='room_name' id="+Room_names+" onclick='nextPage(this.id)'># </div><hr>";
+       document.getElementById("output").innerHTML += row;
       //Start code
 
 
@@ -34,7 +37,15 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       });});}
 getData();
 
+function nextPage(name){
+      console.log("room_name "+ name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+
+}
+
 function logout() {
+      localStorage.removeItem("room_name");
       localStorage.removeItem("user_name");
       window.location = "index.html";
 }
